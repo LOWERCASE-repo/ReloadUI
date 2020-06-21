72a2275e4c6b6ac7950d29755315597022c1ed11
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2020-06-21 16:51:03 -0400

    charge bettered

--- Miracell.cs
+++ Miracell.cs
@@ -18,7 +18,7 @@ class Miracell : MonoBehaviour {
 	private Color color;
 	
 	private AnimationCurve curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
-	private Vector3 pulseMax = new Vector3(0.8f, 0.8f, 1f);
+	private SpriteRenderer[] dividers;
 	private int clip;
 	private bool locked, queued;
 	private float chargeAng;
@@ -26,12 +26,14 @@ class Miracell : MonoBehaviour {
 	private void Start() {
 		clip = size;
 		float gap = 360f / size;
-		chargeAng = 360f + gap / 2f;
+		chargeAng = 720f + gap / 2f;
 		ring.sharedMaterial.SetFloat("_Start", 0f);
 		SetFill(1f);
 		ring.color = color;
+		dividers = new SpriteRenderer[size];
 		for (int i = 0; i < size; i++) {
-			Instantiate(divider, Vector3.zero, (i * gap).Rot(), transform);
+			Quaternion rot = (i * gap).Rot();
+			dividers[i] = Instantiate(divider, Vector3.zero, rot, transform);
 		}
 		StartCoroutine(Drain());
 	}
@@ -69,13 +71,14 @@ class Miracell : MonoBehaviour {
 	}
 	
 	private IEnumerator Beam() {
-		locked = true;
-		while (Input.GetKey(KeyCode.Mouse0)) {
-			pulse.transform.localScale = Vector3.one;
-			yield return new WaitForFixedUpdate();
-		}
-		pulse.transform.localScale = pulseMax;
-		locked = false;
+		// locked = true;
+		// while (Input.GetKey(KeyCode.Mouse0)) {
+		// 	pulse.transform.localScale = new Vector3(0.3f, 0.3f, 1f);
+		// 	yield return new WaitForFixedUpdate();
+		// }
+		// ScalePulse(0f);
+		// locked = false;
+		yield break;
 	}
 	
 	private IEnumerator Drain() {
@@ -88,30 +91,18 @@ class Miracell : MonoBehaviour {
 		}
 		start = Time.fixedTime;
 		i = 0f;
-		clear.localScale = Vector3.zero;
-		pulse.transform.localScale = Vector3.zero;
 		locked = true;
-		float drainLockSeg = drainLockTime / 3f;
 		while (i < drainLockTime) {
 			i = Time.fixedTime - start;
-			float scale;
-			if (i < drainLockSeg * 2f) {
-				scale = curve.Evaluate(i / (drainLockSeg / 2f)) * 0.8f;
-				pulse.transform.localScale = new Vector3(scale, scale, 1f);
-			} else {
-				clip = size;
-				SetFill(1f);
-				pulse.transform.localScale = pulseMax;
-				scale = curve.Evaluate((i - 0.1f) / 0.2f) * 0.5f;
-				clear.localScale = new Vector3(scale, scale, 1f);
-			}
-			pulse.color = Color.Lerp(color, color.gamma, 1f - i / 0.3f);
+			float time = i / drainLockTime;
+			ScaleColor(1f - time);
+			ScalePulse(time);
 			yield return new WaitForFixedUpdate();
 		}
+		ScalePulse(0f);
+		SetFill(1f);
+		clip = size;
 		locked = false;
-		pulse.color = color;
-		pulse.transform.localScale = pulseMax;
-		clear.localScale = pulseMax;
 		StartCoroutine(Drain());
 	}
 	
@@ -121,40 +112,39 @@ class Miracell : MonoBehaviour {
 		float i = 0f;
 		float fillDelta = 1f - ((float)clip / size);
 		bool cancelled = false;
+		float chargeTimeSeg = chargeTime - chargeLockTime;
 		while (i < chargeTime && i >= 0f) {
 			if (!Input.GetKey(KeyCode.Mouse0)) cancelled = true;
 			float delta = Time.fixedTime - start;
-			i = !cancelled ? i + delta : i - 3f * delta;
-			float eval = curve.Evaluate(i / chargeTime);
-			transform.rotation = (chargeAng * eval).Rot();
-			SetFill((float)clip / size + eval * fillDelta);
-			ring.color = Color.Lerp(color, color.gamma, eval);
+			i = !cancelled ? i + delta : i - Mathf.PI * delta;
+			ScaleColor(i / chargeTime);
+			if (i < chargeTimeSeg) {
+				float eval = curve.Evaluate(i / chargeTimeSeg);
+				transform.rotation = (chargeAng * eval).Rot();
+				SetFill((float)clip / size + eval * fillDelta);
+			} else {
+				ScaleDividers(1f - (i - chargeTimeSeg) / (chargeTime - chargeTimeSeg));
+			}
 			start = Time.fixedTime;
 			yield return new WaitForFixedUpdate();
 		}
 		if (!cancelled) {
 			start = Time.fixedTime;
 			i = 0f;
-			ResetPulse();
-			float chargeLockSeg = chargeLockTime / 3f;
+			SetFill(0f);
 			while (i < chargeLockTime) {
 				i = Time.fixedTime - start;
-				float scale;
-				if (i < chargeLockSeg * 2f) {
-					scale = 0.5f - curve.Evaluate(i / (chargeLockSeg * 2f)) * 0.5f;
-					clear.localScale = new Vector3(scale, scale, 1f);
-				} else {
-					SetFill(0f);
-					scale = 0.8f - curve.Evaluate((i - chargeLockSeg) / (chargeLockSeg * 2f)) * 0.8f;
-					pulse.transform.localScale = new Vector3(scale, scale, 1f);
-				}
-				pulse.color = Color.Lerp(color, color.gamma, 1f - i / 0.3f);
+				ScalePulse(1f - i / chargeLockTime);
+				ScaleColor(1f - i / chargeLockTime);
 				yield return new WaitForFixedUpdate();
 			}
-			ResetPulse();
-			transform.rotation = 0f.Rot();
-			clip = 0; // cannot use charge to queue orb
+			ScaleColor(0f);
+			ScalePulse(0f);
+			SetFill(0f);
+			ScaleDividers(1f);
+			clip = 0;
 		}
+		transform.rotation = 0f.Rot();
 		locked = false;
 	}
 	
@@ -162,16 +152,29 @@ class Miracell : MonoBehaviour {
 		ring.sharedMaterial.SetFloat("_Fill", fill);
 	}
 	
-	private void ScrollColor(float time) {
-		
+	private void ScaleColor(float time) {
+		time = curve.Evaluate(time);
+		pulse.color = Color.Lerp(color, color.gamma, time);
+		ring.color = Color.Lerp(color, color.gamma, time);
 	}
 	
-	private void ScrollPulse(float time) {
-		
+	private void ScalePulse(float time) {
+		float scale;
+		if (time < 2f / 3f) {
+			clear.localScale = new Vector3(0f, 0f, 1f);
+			scale = curve.Evaluate(time / 2f * 3f) * 0.8f;
+			pulse.transform.localScale = new Vector3(scale, scale, 1f);
+		} else {
+			pulse.transform.localScale = new Vector3(0.8f, 0.8f, 1f);
+			scale = curve.Evaluate((time - 1f / 3f) / 2f * 3f) * 0.5f;
+			clear.localScale = new Vector3(scale, scale, 1f);
+		}
 	}
 	
-	private void ResetPulse() {
-		clear.localScale = pulseMax;
-		pulse.transform.localScale = pulseMax;
+	private void ScaleDividers(float time) {
+		time = curve.Evaluate(time);
+		foreach (SpriteRenderer divider in dividers) {
+			divider.transform.localScale = new Vector3(0.1f * time, 0.4f, 1f);
+		}
 	}
 }
